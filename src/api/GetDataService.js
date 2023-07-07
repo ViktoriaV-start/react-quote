28bef14eb70728a807fe5f7273deba54959a164f
ViktoriaV <august.flamenco@gmail.com>
2023-07-07 21:06:22 +0300

    correction - link, StrictMode, credentials

--- src/api/GetDataService.js
+++ src/api/GetDataService.js
@@ -4,7 +4,7 @@ import { url } from '../config/constants';
 export class GetDataService  {
 
   static async getData(route=url) {
-      const response = await fetch(`${route}`, { credentials:"include" });
+      const response = await fetch(`${route}`);
       const parser = new DOMParser();
       return parser.parseFromString(await response.text(), 'text/xml');
   }
--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
-export const url = 'https://api.forismatic.com/api/1.0/?method=getQuote&amp;key=random&amp;format=jsonp&amp;lang=ru&amp;jsonp=?';
+export const url = 'https://api.forismatic.com/api/1.0/?method=getQuote&key=random&format=xml&lang=ru';
 export const textError = 'Что-то пошло не так... Попробуйте позже';
 export const backgroundColor = ['background1', 'background2', 'background3', 'background4', 'background5', 'background5', 'background5'];
