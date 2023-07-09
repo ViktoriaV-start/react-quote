967fbab477f9170cbd266f2c07251318c05ad85c
ViktoriaV <august.flamenco@gmail.com>
2023-07-09 15:40:23 +0300

    correction

--- src/api/GetDataService.js
+++ src/api/GetDataService.js
@@ -4,8 +4,27 @@ import { URL } from '../config/constants';
 export class GetDataService  {
 
   static async getData(route = URL) {
-      const response = await fetch(`${route}`);
+      const response = await fetch(route);
       const parser = new DOMParser();
       return parser.parseFromString(await response.text(), 'text/xml');
   }
 }
+
+// ВТОРОЙ ВАРИАНТ ЗАПРОСА - POST метод,
+// также работает только в режиме блокировки CORS ограничений
+
+// const url = 'https://api.forismatic.com/api/1.0/';
+// export class GetDataService  {
+
+//   static async getData(route = url) {
+//       const response = await fetch(route, {
+//         method: 'POST',
+//         headers: {
+//           "Content-Type": "application/x-www-form-urlencoded",
+//       },
+//       body: 'method=getQuote&key=random&format=xml&lang=ru',
+//       });
+//       const parser = new DOMParser();
+//       return parser.parseFromString(await response.text(), 'text/xml');
+//   }
+// }
